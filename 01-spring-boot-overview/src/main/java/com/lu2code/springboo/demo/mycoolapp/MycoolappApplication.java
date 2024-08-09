b5ef5e4ba14d347cbe8673e73949709d8435598d
binhnguyen96html <binhnguyenbiz96@gmail.com>
2024-08-08 17:46:00 -0700

    1st

---
+++ 01-spring-boot-overview/src/main/java/com/lu2code/springboo/demo/mycoolapp/MycoolappApplication.java
@@ -0,0 +1,14 @@
+package com.lu2code.springboo.demo.mycoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycoolappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycoolappApplication.class, args);
+
+	}
+
+}
---
+++ 01-spring-boot-overview/src/test/java/com/lu2code/springboo/demo/mycoolapp/MycoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lu2code.springboo.demo.mycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
