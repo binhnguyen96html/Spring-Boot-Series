70f912b91f2193c11d2bf046a3b778632ae31f3f
binhnguyen96html <binhnguyenbiz96@gmail.com>
2024-08-16 23:43:12 -0700

    update global-superstore for 3 layers

--- global-superstore/src/main/java/com/ltp/globalsuperstore/StoreController.java
+++ global-superstore/src/main/java/com/ltp/globalsuperstore/StoreController.java
@@ -1,10 +1,5 @@
 package com.ltp.globalsuperstore;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
@@ -18,13 +13,15 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 public class StoreController {
 
-  List<Item> items = new ArrayList<>();
+  StoreService storeService = new StoreService();
 
   @GetMapping("/")
   public String getForm(Model model, @RequestParam(required=false) String id){
 
-    model.addAttribute("item", getIndexFromId(id) == Constants.NOT_FOUND ? new Item() : items.get(getIndexFromId(id)));
-    model.addAttribute("categories", Constants.CATEGORIES);
+
+    // model.addAttribute("categories", Constants.CATEGORIES);
+
+    model.addAttribute("item", storeService.getItemfromId(id));
 
     return "form";
   }
@@ -38,16 +35,7 @@ public class StoreController {
 
     if(result.hasErrors()) return "form";
 
-    int index = getIndexFromId(item.getId());
-    String status = Constants.SUCCESS_STATUS;
-
-    if(index == Constants.NOT_FOUND){
-      items.add(item);
-    }else if(within5Days(item.getDate(), items.get(index).getDate())){
-      items.set(index, item);
-    }else{
-      status = Constants.FAILED_STATUS;
-    }
+    String status = storeService.handleSubmit(item);
 
     redirectAttributes.addFlashAttribute("status", status);
 
@@ -58,23 +46,12 @@ public class StoreController {
   @GetMapping("/inventory")
   public String getInventory(Model model){
 
-    model.addAttribute("items", items);
+    model.addAttribute("items", storeService.getItems());
     
     return "inventory";
   }
   
-  public int getIndexFromId(String id){
-    for(int i = 0; i < items.size(); i++){
-      if(items.get(i).getId().equals(id)) return i;      
-    }
-    return Constants.NOT_FOUND;
-  }
 
-  public boolean within5Days(Date newDate, Date oldDate){
 
-    long diff = Math.abs(newDate.getTime() - oldDate.getTime());
-
-    return (int) (TimeUnit.MILLISECONDS.toDays(diff)) <= 5;
-  }
   
 }
